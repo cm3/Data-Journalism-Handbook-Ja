ad104e3cafb63f6103d8ad9eac1cf4366ae70f1b
KAMEDA Akihiro <cm3ak@outlook.com>
2016-11-28 23:09:47 +0900

    fix bug and now all.html updated correctly

--- update.sh
+++ update.sh
@@ -5,14 +5,14 @@ source activate py35con
 COLOR_1="\e[32m"
 COLOR_OFF="\e[m"
 echo -e "${COLOR_1}pull from Transifex ... \n say 'no' to next question!${COLOR_OFF}"
-yes "no" | tx pull -f -l ja_JP
+yes "no" | /usr/local/bin/tx pull -f -l ja_JP
 python3 insert-translators.py
 echo -e "${COLOR_1}render asciidoc ... ${COLOR_OFF}"
 cd jekyll
 /usr/local/bin/asciidoctor -b html5 -o "book.html" index.adoc
-/usr/local/bin/asciidoctor -b html5 -o "../DataJournalismJP.github.io/handbook/all.html" -d book -a toc2 index.adoc
 python3 jekyllify.py
 /usr/local/bin/jekyll build --destination "../DataJournalismJP.github.io/handbook"
+/usr/local/bin/asciidoctor -b html5 -o "../DataJournalismJP.github.io/handbook/all.html" -d book -a toc2 index.adoc
 cd ../
 echo Done.
 echo `date`
